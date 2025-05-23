0f5a26fa784269dc944ebe6108ffe59ed06d65b1
Emmanuel .A <asaoluemma@gmail.com>
2025-05-23 12:39:47 +0100

    Update asteria.ts

--- mesh-sdk/transactions/admin/deploy/asteria.ts
+++ mesh-sdk/transactions/admin/deploy/asteria.ts
@@ -1,36 +1,54 @@
 import {
     admin_token, 
+    initial_fuel, 
     max_asteria_mining, 
+    min_asteria_distance, 
     ship_mint_lovelace_fee 
 } from "../../../const.js";
-import { asteriaScriptAppliedParam } from "../apply-param/Asteria.js";
+import { 
+    Asset, 
+    conStr0, 
+    MeshTxBuilder,
+    scriptHash 
+} from "@meshsdk/core";
 import { blockchainProvider, myWallet } from "../../../utils.js";
-import { Asset, MeshTxBuilder } from "@meshsdk/core";
-import { deployScriptAppliedParam } from "../apply-param/deploy.js";
+import { applyParamtoAsteria } from "../apply-param/Asteria.js";
+import { applyParamtoDeploy } from "../apply-param/deploy.js";
 import { resolvePlutusScriptAddress} from "@meshsdk/core-csl";
-import { writeFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 
 const utxos = await myWallet.getUtxos();
 const changeAddress = await myWallet.getChangeAddress();
 
+const pelletDeployScript = JSON.parse(
+    await readFile("./scriptref-hash/pellet-script.json", "utf-8"));
+if(!pelletDeployScript.txHash){
+    throw Error ("pellet script-ref not found, deploy pellet first.");
+};
+const pelletScriptUtxo = await blockchainProvider.fetchUTxOs(pelletDeployScript.txHash,0);
+const pelletScriptHash = pelletScriptUtxo[0].output.scriptHash;
 
-const asteria = asteriaScriptAppliedParam(
+const asteria = applyParamtoAsteria(
+    scriptHash(pelletScriptHash!),
     admin_token,
     ship_mint_lovelace_fee,
-    max_asteria_mining
+    max_asteria_mining,
+    min_asteria_distance,
+    initial_fuel
 );
-const deployScript = deployScriptAppliedParam(
+const deployScript = applyParamtoDeploy(
     admin_token
 );
 
-const deployScriptAddressBech32 = resolvePlutusScriptAddress(deployScript.deployPlutusScript,0);
+const deployScriptAddressBech32 = resolvePlutusScriptAddress(deployScript.plutusScript,0);
 
 const asteriaAsset: Asset[] = [
     {
       unit: "lovelace",
-      quantity:"10615530"
+      quantity:"30615530"
     }
 ];
+
 
 async function deployAsteria(){
 
@@ -42,18 +60,19 @@ async function deployAsteria(){
     
     const unsignedTx = await txBuiler
     .txOut(deployScriptAddressBech32,asteriaAsset)
-    .txOutReferenceScript(asteria.appliedAsteriaParam,"V3")
+    .txOutInlineDatumValue(conStr0([]),"JSON")
+    .txOutReferenceScript(asteria.cborScript,"V3")
     .selectUtxosFrom(utxos)
     .changeAddress(changeAddress)
     .setNetwork("preprod")
     .complete();
     
     const signedTx = await myWallet.signTx(unsignedTx);
-    const deployAsteriaTx = await myWallet.submitTx(signedTx);
+    const txHash = await myWallet.submitTx(signedTx);
     
 await writeFile(
     "./scriptref-hash/asteria-script.json",
-    JSON.stringify({ txHash: deployAsteriaTx })
+    JSON.stringify({ txHash: txHash })
   );
 };
 
