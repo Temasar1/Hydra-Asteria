c1d7ac93e0195711439b41ae0dcbf8ccf7bbc2f2
Emmanuel .A <asaoluemma@gmail.com>
2025-05-23 12:36:36 +0100

    Update Asteria.ts

--- mesh-sdk/transactions/admin/apply-param/Asteria.ts
+++ mesh-sdk/transactions/admin/apply-param/Asteria.ts
@@ -1,35 +1,46 @@
 
 import { applyParamsToScript } from "@meshsdk/core-cst";
+
+
+
 import plutusBlueprint from "../../../../onchain/src/plutus.json" with {type: 'json'};
-import { Integer, PlutusScript} from "@meshsdk/core";
+
+
+import { Integer, PlutusScript, ScriptHash} from "@meshsdk/core";
  
 const asteriaValidator = plutusBlueprint.validators.find(
         ({ title }) => title === "asteria.asteria.spend"
   );
 const ASTERIA_SCRIPT = asteriaValidator!.compiledCode;
 
-function asteriaScriptAppliedParam(
+function applyParamtoAsteria(
+  pelletScriptAddress: ScriptHash,
   admin_token:any,
   ship_mint_lovelace_fee:Integer,
-  max_asteria_mining: Integer
+  max_asteria_mining: Integer,
+  min_asteria_distance: Integer,
+  initial_fuel: Integer
   ){
 
-  const appliedAsteriaParam   = applyParamsToScript(
+  const cborScript = applyParamsToScript(
      ASTERIA_SCRIPT,
       [
+        pelletScriptAddress,
         admin_token,
         ship_mint_lovelace_fee,
-        max_asteria_mining
+        max_asteria_mining,
+        min_asteria_distance,
+        initial_fuel
       ],
       "JSON"
-      );
+  );
 
-    const asteriaPlutusScript: PlutusScript = {
-      code: appliedAsteriaParam,
+    const PlutusScript: PlutusScript = {
+      code: cborScript,
       version: "V3"
     };
 
-  return { asteriaPlutusScript, appliedAsteriaParam};
+  return { PlutusScript, cborScript};
 };
 
-export{asteriaScriptAppliedParam};
+export{applyParamtoAsteria};
