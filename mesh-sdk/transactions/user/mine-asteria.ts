bef28f4153ec22c951143ee4dd0597d913a8fdf0
Emmanuel .A <asaoluemma@gmail.com>
2025-05-26 07:21:36 +0100

    import indexaion and formatting

--- mesh-sdk/transactions/user/mine-asteria.ts
+++ mesh-sdk/transactions/user/mine-asteria.ts
@@ -1,7 +1,18 @@
 import { fromScriptRef, resolvePlutusScriptAddress } from "@meshsdk/core-cst";
-import { admintoken,fuelToken} from "../../config.js";
+import { admintoken} from "../../config.js";
 import { blockchainProvider } from "../../utils.js";
-import { Asset, conStr0, conStr1, conStr2, deserializeDatum, integer, MeshTxBuilder, PlutusScript, policyId, stringToHex } from "@meshsdk/core";
+import { 
+    Asset, 
+    conStr0, 
+    conStr1, 
+    conStr2, 
+    deserializeDatum, 
+    integer, 
+    MeshTxBuilder, 
+    PlutusScript, 
+    policyId, 
+    stringToHex 
+} from "@meshsdk/core";
 import { readFile } from "fs/promises";
 
 
