da7d4805ac0530a60fb94589171a2a04eae12354
Emmanuel .A <asaoluemma@gmail.com>
2025-05-26 07:14:52 +0100

    update on create deploy pellet

--- mesh-sdk/transactions/admin/deploy/pellet.ts
+++ mesh-sdk/transactions/admin/deploy/pellet.ts
@@ -1,54 +1,56 @@
 import {
     admin_token
 } from "../../../const.js";
-import { pelletScriptApliedParam } from "../apply-param/pellet.js";
-import { blockchainProvider, myWallet } from "../../../utils.js";
-import { Asset, MeshTxBuilder } from "@meshsdk/core";
-import { deployScriptAppliedParam } from "../apply-param/deploy.js";
+import { applyParamtoPellet } from "../apply-param/pellet.js";
+import { blockchainProvider, maestroprovider, myWallet } from "../../../utils.js";
+import { Asset, conStr0, MeshTxBuilder, mNone, none } from "@meshsdk/core";
+import { applyParamtoDeploy } from "../apply-param/deploy.js";
 import { resolvePlutusScriptAddress} from "@meshsdk/core-csl";
 import { writeFile } from "fs/promises";
 
 const utxos = await myWallet.getUtxos();
 const changeAddress = await myWallet.getChangeAddress();
 
-const pellet = pelletScriptApliedParam(
+const pellet = applyParamtoPellet(
     admin_token,
 );
-const deployScript = deployScriptAppliedParam(
+const deployScript = applyParamtoDeploy(
     admin_token
 );
 
-const deployScriptAddressBech32 = resolvePlutusScriptAddress(deployScript.deployPlutusScript,0);
+const deployAddressBech32 = resolvePlutusScriptAddress(deployScript.plutusScript,0);
 
 const pelletAsset: Asset[] = [
     {
-    unit: "lovelace",
-    quantity:"7594220"
+        unit: "lovelace",
+        quantity:"20000000"
     }
 ];
+console.log(deployAddressBech32);
 
 async function deployPellet(){
-
+    
     const txBuiler = new MeshTxBuilder({
-        fetcher: blockchainProvider,
-        submitter: blockchainProvider,
+        fetcher: maestroprovider,
+        submitter: maestroprovider,
         verbose: true
     });
     
     const unsignedTx = await txBuiler
-    .txOut(deployScriptAddressBech32,pelletAsset)
-    .txOutReferenceScript(pellet.appliedPelletParam,"V3")
+    .txOut(deployAddressBech32, pelletAsset)
+    .txOutInlineDatumValue(conStr0([]),"JSON")
+    .txOutReferenceScript(pellet.cborScript, "V3")
     .selectUtxosFrom(utxos)
     .changeAddress(changeAddress)
     .setNetwork("preprod")
     .complete();
     
-    const signedTx = await myWallet.signTx(unsignedTx);
-    const deployPelletTx = await myWallet.submitTx(signedTx);
+    const signedTx = await myWallet.signTx(unsignedTx,true);
+    const txHash = await myWallet.submitTx(signedTx);
 
 await writeFile(
     "./scriptref-hash/pellet-script.json",
-    JSON.stringify({ txHash: deployPelletTx })
+    JSON.stringify({ txHash: txHash })
     );
 };
 
