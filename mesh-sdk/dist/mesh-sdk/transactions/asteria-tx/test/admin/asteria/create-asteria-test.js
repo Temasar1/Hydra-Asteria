818ee94beabfea43161e1b318223725e327b20af
Emmanuel .A <asaoluemma@gmail.com>
2025-06-16 09:11:33 +0100

    changes to remove compiled ts code - dist

--- mesh-sdk/dist/mesh-sdk/transactions/asteria-tx/test/admin/asteria/create-asteria-test.js
+++
@@ -1,3 +0,0 @@
-import { createAsteria } from "../../../admin/asteria/create-asteria.js";
-const txHash = await createAsteria();
-console.log(txHash);
--- mesh-sdk/dist/mesh-sdk/transactions/asteria-tx/test/user/create-ship-test.js
+++
@@ -1,6 +0,0 @@
-import { createShip } from "../../user/create-ship.js";
-const createShipTest = async () => {
-    const txHash = await createShip(-8, 3);
-    return txHash;
-};
-//export {createShipTest};
