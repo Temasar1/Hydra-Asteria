aa4080770ece8afd7ee24eebb31c0a774a728299
Emmanuel .A <asaoluemma@gmail.com>
2025-07-05 23:05:38 +0100

    add tsup for build

---
+++ offchain/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['**/*.ts'],
+  format: ['esm'],
+  outDir: 'dist',
+  clean: true,
+  dts: false,
+  splitting: false,
+  sourcemap: true,
+  target: 'es2022',
+  skipNodeModulesBundle: true,
+  noExternal: ['@radix-ui/react-icons', '@babel/core']
+});
