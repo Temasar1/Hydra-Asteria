1b705480dacc53374e5aaa424110c633e9602516
Emmanuel .A <asaoluemma@gmail.com>
2025-07-08 03:17:50 +0100

    update utxo index and time calculation

--- offchain/transactions/user/move-ship.ts
+++ offchain/transactions/user/move-ship.ts
@@ -9,17 +9,20 @@ import {
     PlutusScript, 
     posixTime, 
     serializePlutusScript, 
+    SLOT_CONFIG_NETWORK, 
     stringToHex,
-    UTxO 
-} from "@meshsdk/core";
+    unixTimeToEnclosingSlot,
+    UTxO } from "@meshsdk/core";
 import { 
+    blockchainProvider, 
+    maestroprovider, 
+    myWallet, 
     readScripRefJson, 
     tx_earliest_slot, 
-    tx_latest_slot 
+    tx_latest_slot
 } from "../../utils.js";
-import { fuel_per_step } from "../../config.js";
-import { blockchainProvider, maestroprovider, myWallet } from "../../utils.js";
 import { fromScriptRef, } from "@meshsdk/core-cst";
+import { fuel_per_step } from "../../config.js";
 
 const changeAddress = await myWallet.getChangeAddress();
 const collateral: UTxO = (await myWallet.getCollateral())[0]!;
@@ -49,7 +52,6 @@ const shipyardPolicyid = spacetimeUtxos[0].output.scriptHash;
 const pellettUtxos = await blockchainProvider.fetchUTxOs(pelletDeployScript.txHash);
 const fuelPolicyid  = pellettUtxos[0].output.scriptHash;
 
-//fetch ship utxo for create ship
 const shipUtxo = await blockchainProvider.fetchUTxOs(ship_tx_hash,1);
 const ship = shipUtxo[0];
     if (!ship.output.plutusData){
@@ -71,15 +73,22 @@ const shipDatumShipTokenName: string = shipInputDatum[2].bytes;
 const shipDatumPilotTokenName:string = shipInputDatum[3].bytes;
 const shipDatumLastMoveLatestTime: number = shipInputDatum[4].int;
 
-const ttl = Date.now() + 10 * 60 * 1000;
+const upperBoundTime = Date.now() * 5 * 60 * 1000;
+const lowerBoundTime = Date.now();
+
 const shipOutputDatum = conStr0([
     integer(Number(shipDatumPosX) + delta_X),
     integer(Number(shipDatumPosY) + delta_Y),
     byteString(shipDatumShipTokenName),
     byteString(shipDatumPilotTokenName),
-    posixTime(ttl)
+    posixTime(upperBoundTime)
 ]);
 
+const upperboundSlot = unixTimeToEnclosingSlot(upperBoundTime, SLOT_CONFIG_NETWORK.preprod);
+const lowerBoundSlot = unixTimeToEnclosingSlot(lowerBoundTime, SLOT_CONFIG_NETWORK.preprod)
+
+console.log(upperboundSlot)
+console.log(lowerBoundSlot)
 //get distance and fuel for distance
 function distance (delta_X: number , delta_Y: number){
     return Math.abs(delta_X) + Math.abs(delta_Y);
@@ -89,7 +98,7 @@ function required_fuel (distance:number, fuel_per_step:number){
 };
 
 const movedDistance = distance(delta_X, delta_Y);
-const spentFuel =   required_fuel(movedDistance , fuel_per_step);
+const spentFuel =   required_fuel(movedDistance ,fuel_per_step);
 const fuelTokenName = stringToHex("FUEL");
 
 //defining assets
@@ -100,6 +109,7 @@ const assetsToSpacetime: Asset[] = [{
     unit: fuelPolicyid + fuelTokenName,
     quantity: (Number(shipFuel) - spentFuel).toString()  
 }];
+
 const pilotTokenAsset: Asset [] = [{
     unit: shipyardPolicyid + shipDatumPilotTokenName,
     quantity: "1"
@@ -109,6 +119,12 @@ const moveShipRedeemer = conStr0([
     integer(delta_X),
     integer(delta_Y)
 ]);
+
+console.log(assetsToSpacetime);
+console.log(pilotTokenAsset);
+console.log(spentFuel);
+console.log(ship.output.amount);
+
 const burnfuelRedeemer = conStr1([]);
 
 const txbuilder = new MeshTxBuilder({
@@ -116,7 +132,10 @@ const txbuilder = new MeshTxBuilder({
     submitter: maestroprovider,
     verbose: true
 })
+
 const unsignedTx = await txbuilder
+
+    .txOut(myWallet.getAddresses().baseAddressBech32!,pilotTokenAsset)
     .spendingPlutusScriptV3()
     .txIn(
         ship.input.txHash,
@@ -133,7 +152,6 @@ const unsignedTx = await txbuilder
     .mintTxInReference(pelletDeployScript.txHash,0)
     .mintRedeemerValue(burnfuelRedeemer,"JSON")
 
-    .txOut(myWallet.getAddresses().baseAddressBech32!,pilotTokenAsset)
     .invalidBefore(tx_earliest_slot)
     .invalidHereafter(tx_latest_slot)
     .txInCollateral(
@@ -149,4 +167,5 @@ const  signedTx = await myWallet.signTx(unsignedTx, true);
 const  moveshipTxhash = await myWallet.submitTx(signedTx);
 return moveshipTxhash;
 };
+
 export {moveShip};
