4af213a2a6e1f290b48b7ab09216360a097f8414
Emmanuel .A <asaoluemma@gmail.com>
2025-07-20 11:05:18 +0100

    format document

--- offchain/transactions/admin/pellet/create-pellet.ts
+++ offchain/transactions/admin/pellet/create-pellet.ts
@@ -5,9 +5,13 @@ import {
   PlutusScript,
   scriptHash,
   stringToHex,
-  UTxO
+  UTxO,
 } from "@meshsdk/core";
-import { blockchainProvider, myWallet, readScripRefJson } from "../../../utils.js";
+import {
+  blockchainProvider,
+  myWallet,
+  readScripRefJson,
+} from "../../../utils.js";
 import { admintoken } from "../../../config.js";
 import { fromScriptRef, resolvePlutusScriptAddress } from "@meshsdk/core-cst";
 
@@ -15,73 +19,76 @@ const changeAddress = await myWallet.getChangeAddress();
 const collateral: UTxO = (await myWallet.getCollateral())[0]!;
 const utxos = await myWallet.getUtxos();
 
-const spacetimeDeployScript = await readScripRefJson('spacetimeref');
-if(!spacetimeDeployScript.txHash){
-    throw Error ("spacetime script-ref not found, deploy spacetime first.");
-}; 
-const pelletDeployScript = await readScripRefJson('pelletref');
-if(!pelletDeployScript.txHash){
-  throw Error ("pellet script-ref not found, deploy pellet first.");
-};
+const spacetimeDeployScript = await readScripRefJson("spacetimeref");
+if (!spacetimeDeployScript.txHash) {
+  throw Error("spacetime script-ref not found, deploy spacetime first.");
+}
+const pelletDeployScript = await readScripRefJson("pelletref");
+if (!pelletDeployScript.txHash) {
+  throw Error("pellet script-ref not found, deploy pellet first.");
+}
 
-const pelletUtxo = await blockchainProvider.fetchUTxOs(pelletDeployScript.txHash);
+const pelletUtxo = await blockchainProvider.fetchUTxOs(
+  pelletDeployScript.txHash
+);
 const fuelPolicyID = pelletUtxo[0].output.scriptHash;
 
 const pelletScriptRef = fromScriptRef(pelletUtxo[0].output.scriptRef!);
 const pelletPlutusScript = pelletScriptRef as PlutusScript;
-const pelletScriptAddress = resolvePlutusScriptAddress(pelletPlutusScript,0);
+const pelletScriptAddress = resolvePlutusScriptAddress(pelletPlutusScript, 0);
 
-const spacetimeUtxo = await blockchainProvider.fetchUTxOs(spacetimeDeployScript.txHash);
+const spacetimeUtxo = await blockchainProvider.fetchUTxOs(
+  spacetimeDeployScript.txHash
+);
 const shipyardPolicyId = spacetimeUtxo[0].output.scriptHash;
 
 export async function createPellet(
-  pelletProperty: { posX: number; posY: number; fuel: string }[], totalFuelMint: string,
+  pelletProperty: { posX: number; posY: number; fuel: string }[],
+  totalFuelMint: string
 ) {
- 
-const fueltokenNameHex = stringToHex("FUEL");
-const fuelReedemer = conStr0([]);
+  const fueltokenNameHex = stringToHex("FUEL");
+  const fuelReedemer = conStr0([]);
 
-const txBuilder = new MeshTxBuilder({
+  const txBuilder = new MeshTxBuilder({
     fetcher: blockchainProvider,
     submitter: blockchainProvider,
     evaluator: blockchainProvider,
-    verbose: true
+    verbose: true,
   });
   txBuilder
-  .mintPlutusScriptV3()
-  .mint(totalFuelMint, fuelPolicyID!, fueltokenNameHex)
-  .mintTxInReference(pelletDeployScript.txHash, 0)
-  .mintRedeemerValue(fuelReedemer, "JSON")
+    .mintPlutusScriptV3()
+    .mint(totalFuelMint, fuelPolicyID!, fueltokenNameHex)
+    .mintTxInReference(pelletDeployScript.txHash, 0)
+    .mintRedeemerValue(fuelReedemer, "JSON");
 
   for (const pellet of pelletProperty) {
     const pelletDatum = conStr0([
-      integer(pellet.posX), 
-      integer(pellet.posY), 
-      scriptHash(shipyardPolicyId!)
+      integer(pellet.posX),
+      integer(pellet.posY),
+      scriptHash(shipyardPolicyId!),
     ]);
-    
-  txBuilder.txOut(pelletScriptAddress, [ 
-    {
-      unit: fuelPolicyID + fueltokenNameHex,
-      quantity: pellet.fuel
-    },
-    {
-      unit: admintoken.policyid + admintoken.name,
-      quantity: "1"
-    }]
-  )
-  .txOutInlineDatumValue(pelletDatum, "JSON")
+
+    txBuilder
+      .txOut(pelletScriptAddress, [
+        {
+          unit: fuelPolicyID + fueltokenNameHex,
+          quantity: pellet.fuel,
+        },
+        {
+          unit: admintoken.policyid + admintoken.name,
+          quantity: "1",
+        },
+      ])
+      .txOutInlineDatumValue(pelletDatum, "JSON");
   }
-  txBuilder .txInCollateral(
-    collateral.input.txHash,
-    collateral.input.outputIndex
-  )
-  .changeAddress(changeAddress)
-  .selectUtxosFrom(utxos)
-  .setNetwork("preprod")
-  
+  txBuilder
+    .txInCollateral(collateral.input.txHash, collateral.input.outputIndex)
+    .changeAddress(changeAddress)
+    .selectUtxosFrom(utxos)
+    .setNetwork("preprod");
+
   const unsignedTx = await txBuilder.complete();
   const signedTx = await myWallet.signTx(unsignedTx);
   const pelletTxhash = await myWallet.submitTx(signedTx);
   return pelletTxhash;
-};
+}
