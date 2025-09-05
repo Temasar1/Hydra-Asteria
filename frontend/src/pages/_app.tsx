6d8c46b124007e69b5b9fcd838f7fb4f3969dd6a
Emmanuel .A <asaoluemma@gmail.com>
2025-09-05 22:53:11 +0100

    complete frontend

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,12 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import type { ReactElement, ReactNode } from 'react';
+import { useEffect, useState } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import NavBar from "@/components/NavBar";
 import "./globals.css";
-import { CardanoWallet, MeshProvider } from '@meshsdk/react';
+import dynamic from 'next/dynamic';
+const MeshProvider = dynamic(() => import('@meshsdk/react').then(m => m.MeshProvider), { ssr: false });
+const CardanoWallet = dynamic(() => import('@meshsdk/react').then(m => m.CardanoWallet), { ssr: false });
  
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -17,11 +19,13 @@ type AppPropsWithLayout = AppProps & {
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const showNavBar =  (Component as any).showNavBar;
   const showCardanoWallet = (Component as any).showCardanoWallet;
-  return (
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => setIsClient(true), []);
+  return isClient ? (
     <MeshProvider>
-      {showNavBar && <NavBar /> }
-      {showCardanoWallet && <CardanoWallet/>}
+      {showNavBar && <NavBar />}
+      {showCardanoWallet && <CardanoWallet />}
       <Component {...pageProps} />
     </MeshProvider>
-  );
+  ) : null;
 }
