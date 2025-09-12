df10e5b66b012d37f3a998558d5fc6a9ae9cb4c8
Emmanuel .A <asaoluemma@gmail.com>
2025-09-12 15:46:57 +0100

    change: new file dir.

---
+++ offchain/src/admin/apply-param/deploy.ts
@@ -0,0 +1,26 @@
+
+import { applyParamsToScript } from "@meshsdk/core-cst";
+import plutusBlueprint from "../../../../onchain/src/plutus.json" with {type: 'json'};
+import { PlutusScript } from "@meshsdk/core";
+
+const asteriaValidator = plutusBlueprint.validators.find(
+        ({ title }) => title === "deploy.deploy.spend"
+  );
+
+const DEPLOY_SCRIPT = asteriaValidator!.compiledCode;
+
+function applyParamtoDeploy( admin_token:any){
+    const cborScript   = applyParamsToScript(
+      DEPLOY_SCRIPT,
+      [admin_token],
+      "JSON"
+    );
+
+  const plutusScript : PlutusScript = {
+    code: cborScript,
+    version: "V3"
+  };
+return {cborScript,plutusScript};
+};
+
+export {applyParamtoDeploy};
