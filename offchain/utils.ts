81f2da6d9b61a3798417d3cd2ad7ca3ae38191f2
Emmanuel .A <asaoluemma@gmail.com>
2025-09-12 15:52:19 +0100

    move: secrets to .env

--- offchain/utils.ts
+++ offchain/utils.ts
@@ -1,19 +1,21 @@
 import { MeshWallet, BlockfrostProvider, MaestroProvider} from "@meshsdk/core";
 import { readFile, writeFile } from "fs/promises";
 import { join } from 'path';
+import "dotenv/config";
 
-// const blockfrostApiKey = process.env.BLOCKFROST_APIKEY;
-// //const seedPhrase  = process.env.SEED_PHRASE?.split(',')!
-// if (!blockfrostApiKey) {
-//   throw new Error("BLOCKFROST_APIKEY not found in .env file.");
-// }
-// if (!seedPhrase){
-//   throw new Error("SEED_PHRASE not found in .env file");
-// }
-//const seedPhrase = ["Hockey","Umbrella","Liquid","Robust","Security","Cash","Panther","Buffalo","File","Fiction","Typical","Food","Omit","Example","Valid","Bronze","Mutual","Denial","Lens","System","Pottery","Portion","Swift","Stove"];
-const seedPhrase = ["vibrant","north","decade","mean","ensure","turn","universe","cause","neutral","mad","can","next","mutual","tongue","main","bind","lizard","crumble","order","pole","assault","guilt","physical","cup"];
-export const blockchainProvider = new BlockfrostProvider("preprodYAw21nxr9EdeZNSLDDLOJVg98DOrya75");
-
+const seedPhrase  = process.env.SEED_PHRASE?.split(' ');
+if(seedPhrase == undefined){
+  throw new Error("Enter your seedphrase in an env file")
+}
+const blockfrost_API = process.env.BLOCKFROST_APIKEY;
+if(blockfrost_API == undefined){
+  throw new Error("Enter your blockfrost key in an env file")
+}
+const maestro_API = process.env.MAESTRO_APIKEY;
+if(maestro_API == undefined){
+  throw new Error("Enter your maestro key in an env file")
+}
+export const blockchainProvider = new BlockfrostProvider(blockfrost_API);
 export const myWallet = new MeshWallet({
   networkId: 0, // 0: testnet, 1: mainnet
   fetcher: blockchainProvider,
@@ -26,7 +28,7 @@ export const myWallet = new MeshWallet({
 
 export const maestroprovider = new MaestroProvider({
   network: "Preprod",
-  apiKey: "HOMTwVrArW5p3LEsv69qJ32gJ9q6xP4u",
+  apiKey: maestro_API,
   turboSubmit:false
 });
 
@@ -39,7 +41,7 @@ export const tx_earliest_slot = Number(slot) - 60;
 //export const tx_earliest_posix_time = time - 60 * 1000;     //- 1 minute from now
 
 const __dirname = (process.cwd())
-const __filedir = join(__dirname, '/offchain/transactions/admin/deploy/ref-script/');
+const __filedir = join(__dirname, '/src/admin/deploy/ref-script/');
 
 export const writeScriptRefJson = async (filename: string, txHash: string) => {
   await writeFile(
