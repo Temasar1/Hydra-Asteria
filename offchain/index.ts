e81f85eb9f3d4ab03b7efa9e83123e7a8e52cd46
Emmanuel .A <asaoluemma@gmail.com>
2025-09-25 05:15:59 +0100

    update index.ts

--- offchain/index.ts
+++ offchain/index.ts
@@ -1,17 +1,17 @@
-import { CardanoSubmitClient } from "@utxorpc/sdk";
-import { createPelletTest } from "./src/admin/pellet/create-pellet-test.js";
-import { createShip } from "./src/user/create-ship.js";
-import { moveShip } from "./src/user/move-ship.js";
-import { quit } from "./src/user/quit.js";
-import { consumePellets } from "./src/admin/pellet/consume-pellet.js";
-import { gatherFuel } from "./src/user/gather-fuel.js";
+import { deployAsteria } from "./src/admin/deploy/asteria.js";
+import { deployPellet } from "./src/admin/deploy/pellet.js";
+
+const txHash = await deployPellet();
+console.log(txHash);
+
+//In every game interaction, ship utxo is being created
+//First step deploy pellet then deploy following validators
 
 
-//const txHash = await moveShip(2,1,"f84b61cf88ce198781feb1409803cb618af4bfa186e031aeccb58632832d0ceb");
-//const txHash = await createPelletTest(5);
-//const txHash = await createShip(14,30);
-const txHash = await gatherFuel("ff689c0e0826e0bf62ed893cec4aa2ecc72580c2eb953cadb5edc60d5f370935","9b48ac9d761f3e8e990404d0cad15e2e351e53299e22c04842df71ad68a690ed",1)
-
-//const txHash = await consumePellets("1b9d6ab2c74cea07728f8fc96df2227b289374978e8dded0dbc7c24fb6f45dfb")
-// const txHash = await quit("d4e6194245a4b36eeed4f79f4df8eb05aa99f92d64bcfaa952ede5430877b087")
-console.log(txHash)
+//de1ecb8f79167eaa794fb6b8b20d81440ca37409692b3ca7a8abc12b434af095 - asteria
+//76bc7d608adc0b5fde73fe75936e296a0d187fc3d6511f4508e7623d55e3ff9f - pellet 
+//7a7b5c8dedfcce1f3c6df5b2dea95ec1e8f8d566d6f1bd62bba16bd8d927a22c - ship 
+//61cc4486ec153ab716065b9631511f86d4bc85038aa80eb1789756baceabdb2c - moveship 
+//898c9a5a8bdfff9e14aa642832a2d45e15fde2a40d252265ff774f4e549eee6e - gatherFuel
+//baed49f4c5f0395de8d86c8a93f0a695056b4bcd3d94c6defd4cbe5a58a52d38 - mineAsteria
+//7583c87adb8e0ae938c3583929411585a9861e31fe1d21128883c4c75fd7702a - consumeAsteria
